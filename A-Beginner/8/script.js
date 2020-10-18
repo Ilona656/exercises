51fdf65eff1b74af729c360ed528926fc0230cb4
Ilona Badur <Ilona.badur@skola.botkyrka.se>
2020-10-18 13:32:38 +0200

    solve8

---
+++ A-Beginner/8/script.js
@@ -0,0 +1,7 @@
+// Skapa ett objekt som representerar dig själv och som innehåller properties `firstName`, `lastName` och `age`
+
+const me = {
+    firstName: 'Ilona',
+    lastName: 'Badur',
+    age: '17'
+}
