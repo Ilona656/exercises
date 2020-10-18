50f2b197b94a5fd19484473a5e49a7077846fc1c
Ilona Badur <Ilona.badur@skola.botkyrka.se>
2020-10-18 14:28:47 +0200

    solve15

---
+++ A-Beginner/15/scripts.js
@@ -0,0 +1,11 @@
+// 15. Skriv en funktion som multiplicerar två tal och returnerar resultatet
+function multiply(x, y) {
+    return x * y
+}
+
+const a = 5
+const b = 7
+
+const produkt = multiply(a, b)
+
+console.log(a + '*' + b + '=' + produkt)
