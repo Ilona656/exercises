a244403280ca7f42e364531da626e850bfff70ab
Ilona Badur <Ilona.badur@skola.botkyrka.se>
2020-10-18 16:27:07 +0200

    solve19

---
+++ A-Beginner/19/script.js
@@ -0,0 +1,5 @@
+// 19. Skriv en funktion ``isLong`` som accepterar en string och returnerar `true` ifall den är längre än 18 tecken annars `false`
+
+function isLong(str) {
+    return length.str > 18
+}
